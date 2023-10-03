f07d7c7b0dbb263395d7c57df2821ffbd0554e77
Valerii <valerii.ovchinnikov@jetbrains.com>
2023-10-03 20:47:44 +0200

    update version to support new ui

--- basic_plugin/build.gradle.kts
+++ basic_plugin/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
+    version.set("2023.1.2")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("com.intellij.java"/* Plugin Dependencies */))
